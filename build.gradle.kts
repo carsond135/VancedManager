b056447d8dadb3b14ded59b36d1472358f7c2ee8
Carson B Dunlap <cbd135.dev@gmail.com>
2022-03-15 02:21:31 -0400

    first round of files for upload

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+buildscript {
+    val kotlinVersion by extra("1.5.30")
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:perf-plugin:1.4.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0-alpha10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
